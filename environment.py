022fa4af0a07dc32968e4e019dd71d7507914978
raihanarifmustofa <raihanqyub@gmail.com>
2026-09-03 22:40:57 +0700

    Update environment.py

--- environment.py
+++ environment.py
@@ -4,7 +4,7 @@ import django
 
 def before_all(context):
     # Path to directory (Isi sesuai direktori kalian)
-    project_path = r"D:\1. Rehan\0. KULIAH\2. MATA KULIAH\SEMESTER 5\11. Pembangunan Perangkat Lunak Praktikum"
+    project_path = r"PROJECT PATH"
 
     # Add project path to sys.path
     sys.path.insert(0, project_path)
